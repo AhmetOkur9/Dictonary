2117ef910f65554cfe3f2485b8e99c717105c1f6
Ahmet <Ahmet@DESKTOP-H7889DL>
2022-10-12 13:17:14 +0300

    Add project files.

---
+++ Dictonary/MyDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictonary
+{
+    class MyDictionary<T>
+    {
+        T[] things;
+        public MyDictionary()
+        {
+            things = new T[0];
+        }
+        public void Add(T thing)
+        {
+            T[] tempArray = things;
+            things = new T[things.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                things[i] = tempArray[i];
+            }
+
+            things[things.Length - 1] = thing;
+        }
+
+    }
+}
---
+++ Dictonary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kodlar MyDictionary class'ında.
+            MyDictionary<string> dictionary = new MyDictionary<string>();        
+        }
+    }
+}
